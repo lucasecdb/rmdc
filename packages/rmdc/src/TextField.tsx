3dd56b9c1607f6efc0eba899b6b7d7a8a237134b
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-11-16 16:29:33 -0300

    add label and notch adapters

--- packages/rmdc/src/TextField.tsx
+++ packages/rmdc/src/TextField.tsx
@@ -1,12 +1,24 @@
 import {
   MDCTextFieldFoundation,
+  MDCTextFieldInputAdapter,
+  MDCTextFieldLabelAdapter,
   MDCTextFieldLineRippleAdapter,
+  MDCTextFieldOutlineAdapter,
   MDCTextFieldRootAdapter,
+  cssClasses,
 } from '@material/textfield'
 import classNames from 'classnames'
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react'
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 
 import useClassList from './hooks/useClassList'
+import useLatestRef from './hooks/useLatestRef'
 import { NotchedOutline } from './NotchedOutline'
 import { FloatingLabel } from './FloatingLabel'
 import { LineRipple } from './LineRipple'
@@ -15,16 +27,29 @@ const noop = () => {}
 
 interface ContextType {
   textarea: boolean
+  outlined: boolean
+  registerLabel: (ref: any) => void
+  labelFloat: boolean
+  onLabelWidthChange: (width: number) => void
+  notchActive: boolean
+  notchWidth: number
 }
 
 const ctx = React.createContext<ContextType>({
   textarea: false,
+  outlined: false,
+  registerLabel: noop,
+  onLabelWidthChange: noop,
+  labelFloat: false,
+  notchActive: false,
+  notchWidth: 0,
 })
 
 export interface TextFieldProps extends React.HTMLAttributes<HTMLDivElement> {
   fullWidth: boolean
   outlined: boolean
   textarea: boolean
+  noLabel: boolean
 }
 
 export interface TextFieldLabelProps {
@@ -37,17 +62,40 @@ export interface TextFieldInputProps {
 }
 
 export const Label: React.FC<TextFieldLabelProps> = ({ children }) => {
-  const { textarea } = useContext(ctx)
+  const {
+    textarea,
+    outlined,
+    labelFloat,
+    registerLabel,
+    onLabelWidthChange,
+    notchActive,
+    notchWidth,
+  } = useContext(ctx)
+  const labelRef = useRef<any | null>(null)
 
-  if (textarea) {
+  useEffect(() => {
+    registerLabel(labelRef.current)
+  }, [registerLabel])
+
+  const labelElement = (
+    <FloatingLabel
+      ref={labelRef}
+      float={labelFloat}
+      onWidthChange={onLabelWidthChange}
+    >
+      {children}
+    </FloatingLabel>
+  )
+
+  if (textarea || outlined) {
     return (
-      <NotchedOutline>
-        <FloatingLabel>{children}</FloatingLabel>
+      <NotchedOutline notch={notchActive} notchWidth={notchWidth}>
+        {labelElement}
       </NotchedOutline>
     )
   }
 
-  return <FloatingLabel>{children}</FloatingLabel>
+  return labelElement
 }
 
 export const Input: React.FC<TextFieldInputProps> = () => {
@@ -73,6 +121,7 @@ export const TextField: React.FC<TextFieldProps> = ({
   fullWidth = false,
   outlined = false,
   textarea = false,
+  noLabel = false,
   className,
   children,
   ...props
@@ -82,11 +131,19 @@ export const TextField: React.FC<TextFieldProps> = ({
   const { addClass, removeClass, classList } = useClassList()
   const foundationRef = useRef<MDCTextFieldFoundation | null>(null)
 
-  const classListRef = useRef(classList)
+  const [isFocused, setFocused] = useState(false)
+  const [labelFloat, setLabelFloat] = useState(false)
+  const labelRef = useRef<any | null>(null)
+  const [labelWidth, setLabelWidth] = useState(0)
+  const [notchActive, setNotchActive] = useState(false)
+  const [notchWidth, setNotchWidth] = useState(0)
 
-  useEffect(() => {
-    classListRef.current = classList
-  }, [classList])
+  const classListRef = useLatestRef(classList)
+  const labelWidthRef = useLatestRef(labelWidth)
+  const outlinedRef = useLatestRef(outlined)
+  const textareaRef = useLatestRef(textarea)
+  const nativeInputRef = useLatestRef(null)
+  const isFocusedRef = useLatestRef(isFocused)
 
   useEffect(() => {
     const lineRippleAdapter: MDCTextFieldLineRippleAdapter = {
@@ -94,6 +151,40 @@ export const TextField: React.FC<TextFieldProps> = ({
       deactivateLineRipple: () => setLineRippleActive(false),
       setLineRippleTransformOrigin: normalizedX =>
         setLineRippleOrigin(normalizedX),
+    }
+
+    const labelAdapter: MDCTextFieldLabelAdapter = {
+      shakeLabel: shouldShake => {
+        if (shouldShake) {
+          labelRef.current?.shake()
+        }
+      },
+      hasLabel: () => !!labelRef.current,
+      floatLabel: shouldFloat => {
+        setLabelFloat(shouldFloat)
+      },
+      getLabelWidth: () => labelWidthRef.current,
+    }
+
+    const notchAdapter: MDCTextFieldOutlineAdapter = {
+      hasOutline: () => {
+        return outlinedRef.current || textareaRef.current
+      },
+      closeOutline: () => {
+        setNotchActive(false)
+      },
+      notchOutline: labelWidth => {
+        setNotchActive(true)
+        setNotchWidth(labelWidth)
+      },
+    }
+
+    const inputAdapter: MDCTextFieldInputAdapter = {
+      isFocused: () => isFocusedRef.current,
+      getNativeInput: () => nativeInputRef.current,
+
+      registerInputInteractionHandler: noop,
+      deregisterInputInteractionHandler: noop,
     }
 
     const adapter: MDCTextFieldRootAdapter = {
@@ -110,30 +201,78 @@ export const TextField: React.FC<TextFieldProps> = ({
     }
 
     foundationRef.current = new MDCTextFieldFoundation(
-      Object.assign({}, adapter, lineRippleAdapter)
+      Object.assign(
+        {},
+        adapter,
+        lineRippleAdapter,
+        labelAdapter,
+        notchAdapter,
+        inputAdapter
+      )
     )
     foundationRef.current.init()
 
     return () => {
       foundationRef.current?.destroy()
     }
-  }, [addClass, classList, removeClass])
+  }, [
+    addClass,
+    classList,
+    classListRef,
+    isFocusedRef,
+    labelWidthRef,
+    nativeInputRef,
+    outlinedRef,
+    removeClass,
+    textareaRef,
+  ])
 
-  const classes = classNames(className, 'mdc-text-field', {
-    'mdc-text-field--outlined': outlined,
-    'mdc-text-field--fullwidth': fullWidth,
-    'mdc-text-field--textarea': textarea,
+  const classes = classNames(className, cssClasses.ROOT, {
+    [cssClasses.OUTLINED]: outlined,
+    [cssClasses.FULLWIDTH]: fullWidth,
+    [cssClasses.TEXTAREA]: textarea,
+    [cssClasses.NO_LABEL]: noLabel,
+    [cssClasses.FOCUSED]: isFocused,
   })
 
   const handleFocus = () => {
+    setFocused(true)
     foundationRef.current?.activateFocus()
   }
 
   const handleBlur = () => {
+    setFocused(false)
     foundationRef.current?.deactivateFocus()
   }
 
-  const context = useMemo(() => ({ textarea }), [textarea])
+  const registerLabel = useCallback((ref: any) => {
+    labelRef.current = ref
+  }, [])
+
+  const onLabelWidthChange = useCallback((width: number) => {
+    setLabelWidth(width)
+  }, [])
+
+  const context = useMemo(
+    () => ({
+      textarea,
+      outlined,
+      labelFloat,
+      registerLabel,
+      onLabelWidthChange,
+      notchActive,
+      notchWidth,
+    }),
+    [
+      labelFloat,
+      notchActive,
+      notchWidth,
+      onLabelWidthChange,
+      outlined,
+      registerLabel,
+      textarea,
+    ]
+  )
 
   return (
     <ctx.Provider value={context}>
