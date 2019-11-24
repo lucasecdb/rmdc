ebad86db201a8a23d8e12e9465ff31e604b416f5
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-11-24 11:54:50 -0300

    Fix docs title (#11)
    * remove component string from pages title
    
    * add title to pages

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -32,5 +32,6 @@ module.exports = {
         fonts: ['Material+Icons'],
       },
     },
+    'gatsby-plugin-react-helmet',
   ],
 }
--- docs/src/templates/ComponentTemplate.tsx
+++ docs/src/templates/ComponentTemplate.tsx
@@ -1,12 +1,14 @@
-import * as React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import * as React from 'react'
+import { Helmet } from 'react-helmet'
 
 import Layout from '../components/Layout'
 
 const ComponentTemplate: React.FC<any> = ({ data: { mdx } }) => {
   return (
     <Layout>
+      <Helmet title={mdx.frontmatter.title} />
       <div className="mdx-container">
         <MDXRenderer>{mdx.body}</MDXRenderer>
       </div>
